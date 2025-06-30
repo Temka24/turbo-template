13092cc477dade8de5fcaefc7626a120a0463c7d
Temka <thxdeadshotxht@gmail.com>
2025-06-30 21:30:16 +0800

    add doppler yaml

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, new FastifyAdapter());
 
-    await app.listen(5000);
+    await app.listen(process.env.PORT || 5001, "0.0.0.0");
 }
 void bootstrap();
